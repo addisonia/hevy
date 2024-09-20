1dbe0bdad84a92b3b9c713e2528fa8497a62215f
addison atkin <addisonathome@gmail.com>
2024-09-20 10:41:56 -0500

    fixed home button

--- src/components/HevyDashboard.jsx
+++ src/components/HevyDashboard.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../HevyDashboard.css'; // Import the new CSS file
 
 const HevyDashboard = () => {
   return (
     <div className="dashboard">
       <h1>My Hevy Workout Dashboard</h1>
-      <div style={{ height: '60vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        <Link to="/all-exercises" className="btn btn-primary">
+      <div className="button-container">
+        <Link to="/all-exercises" className="dashboard-btn dashboard-btn-primary">
           See All Exercises
         </Link>
       </div>
+      {/* Add more dashboard content here */}
     </div>
   );
 };
